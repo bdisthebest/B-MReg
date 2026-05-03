81f162daeca35ccf8b4fd9332d1562777ce48c55
bdisthebest <brennan_01@comcast.net>
2026-05-03 19:43:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -54,14 +54,14 @@ const siteContent = {
       title: "Honeymoon Fund",
       amount: "Any amount",
       description:
-        "Help us create unforgettable moments on our honeymoon, from special dinners to excursions.",
+        "Help us create unforgettable moments on our honeymoon, from special dinners to excursions. (This will open a paypal link)",
       link: "https://paypal.me/BMwedding2027",
     },
     {
       title: "Home Sweet Home Fund",
       amount: "Any amount",
       description:
-        "Contribute toward the finishing touches that will help us make our home warm and welcoming.",
+        "Contribute toward the finishing touches that will help us make our home warm and welcoming.(This will open a paypal link)",
       link: "https://paypal.me/BMwedding2027",
     },
   ],
